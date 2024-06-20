5b7dcb7d73af1ccb18232f5a0b3a7d824d3aa46e
zaehorang <zaehorang2@naver.com>
2024-06-20 14:54:29 +0900

    feat: Add grid plane model
    - 평면을 감지하고 보여줄 SCNNode 모델

---
+++ AR-Basketball/AR-Basketball/GridPlane.swift
@@ -0,0 +1,45 @@
+//
+//  GridPlane.swift
+//  AR-Basketball
+//
+//  Created by zaehorang on 6/20/24.
+//
+
+import ARKit
+
+final class GridPlane: SCNNode {
+    let node: SCNNode
+    var classificationNode: SCNNode?
+    
+    /// - Tag: VisualizePlane
+    init(anchor: ARPlaneAnchor, in sceneView: ARSCNView) {
+        // 평면의 경계 직사각형을 시각화하기 위한 노드 생성
+        let plane = SCNPlane(width: CGFloat(anchor.planeExtent.width), height: CGFloat(anchor.planeExtent.height))
+        node = SCNNode(geometry: plane)
+        node.simdPosition = anchor.center
+        
+        // `SCNPlane`은 로컬 좌표 공간에서 수직으로 정렬되어 있으므로,
+        // `ARPlaneAnchor`의 방향에 맞추기 위해 회전시킴
+        node.eulerAngles.x = -.pi / 2
+
+        super.init()
+        
+        setupNodeVisualStyle()
+
+        // 노드를 자식 노드로 추가하여 장면에 나타나게 함
+        addChildNode(node)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupNodeVisualStyle() {
+        // 경계 시각화를 반투명하게 하여 실제 배치를 명확하게 보여줌
+        guard let material = node.geometry?.firstMaterial else {
+            fatalError("SCNPlane always has one material")
+        }
+        
+        material.diffuse.contents = UIImage(named: "GridImage.png")
+    }
+}
